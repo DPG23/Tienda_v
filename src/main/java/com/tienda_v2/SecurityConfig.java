5d74ee7db472a0a89730199a2b18dea1cfb30346
Usuario <Usuario@DESKTOP-7GUAGD8>
2023-04-14 17:26:08 -0600

    Semana 13

--- src/main/java/com/tienda_v2/SecurityConfig.java
+++ src/main/java/com/tienda_v2/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.tienda_v2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,64 +17,77 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    /*  @Bean
     public UserDetailsService users() {
         UserDetails admin = User.builder()
-                .username("Juan")
+                .username("juan")
                 .password("{noop}123")
-                .roles("ADMIN", "VENDEDOR", "USER")
+                .roles("USER","VENDEDOR", "ADMIN")
                 .build();
-
-        UserDetails vendedor = User.builder()
+        UserDetails sales = User.builder()
                 .username("rebeca")
                 .password("{noop}456")
-                .roles("VENDEDOR", "USER")
+                .roles("VENDEDOR","USER")
                 .build();
-
-        UserDetails usuario = User.builder()
+        UserDetails user = User.builder()
                 .username("pedro")
                 .password("{noop}789")
                 .roles("USER")
                 .build();
+        
+        return new InMemoryUserDetailsManager(user,sales, admin);
+    } */
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-        return new InMemoryUserDetailsManager(usuario, vendedor, admin);
-
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((request) -> request
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
                 .requestMatchers("/",
                         "/index",
-                        "/login",
                         "/errores/**",
+                        "/carrito/**",
                         "/webjars/**").permitAll()
-                .requestMatchers("/articulo/nuevo",
+                .requestMatchers(
+                        "/articulo/nuevo",
                         "/articulo/guardar",
                         "/articulo/modificar/**",
                         "/articulo/eliminar/**",
+                        "/categoria/nuevo",
+                        "/categoria/guardar",
+                        "/categoria/modificar/**",
+                        "/categoria/eliminar/**",
                         "/cliente/nuevo",
                         "/cliente/guardar",
                         "/cliente/modificar/**",
                         "/cliente/eliminar/**",
-                        "/categoria/nuevo",
-                        "/categoria/guardar",
-                        "/categoria/modificar/**",
-                        "/categoria/eliminar/**")
-                .hasRole("ADMIN")
-                .requestMatchers("/articulo/listado",
+                        "/reportes/**"
+                ).hasRole("ADMIN")
+                .requestMatchers(
+                        "/articulo/listado",
                         "/categoria/listado",
-                        "/cliente/listado")
-                .hasAnyRole("ADMIN", "VENDEDOR")
-        )
+                        "/cliente/listado"
+                ).hasAnyRole("ADMIN", "VENDEDOR")
+                .requestMatchers("/facturar/carrito")
+                .hasRole("USER")
+                )
                 .formLogin((form) -> form
-                .loginPage("/login")
-                .permitAll())
+                .loginPage("/login").permitAll())
                 .logout((logout) -> logout.permitAll())
-                .exceptionHandling().accessDeniedPage("/errores/403");
+                .exceptionHandling()
+                .accessDeniedPage("/errores/403");
         return http.build();
-        
-        
     }
+    
+    
 
 }
+
+
